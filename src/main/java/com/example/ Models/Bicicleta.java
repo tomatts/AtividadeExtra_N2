793d905a4d5aff340603c695569840e98e3089bf
matthew <matt-sn@hotmail.com>
2025-06-09 01:58:21 -0400

    constructors, getters & setters de Bicicleta

--- src/main/java/com/example/ Models/Bicicleta.java
+++ src/main/java/com/example/ Models/Bicicleta.java
@@ -2,10 +2,31 @@ package com.example.Models;
 
 import com.example.Abstractions.IBicicleta;
 
-public class Bicicleta implements IBicicleta{
+public class Bicicleta implements IBicicleta {
     private int velocidade;
     private int marcha;
 
+    // Construtor padrão
+    public Bicicleta() {
+        this.velocidade = 0;
+        this.marcha = 0;
+
+    }
+
+    public int getVelocidade() {
+        return velocidade;
+    }
+
+    public void setVelocidade(int velocidade) {
+        this.velocidade = velocidade;
+    }
+
+    public int getMarcha() {
+        return marcha;
+    }
+
+    public void setMarcha(int marcha) {
+        this.marcha = marcha;
+    }
 
 }
-
--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -2,6 +2,9 @@ package com.example;
 
 import java.util.Scanner;
 
+import com.example.Models.Bicicleta;
+import com.example.Abstractions.IBicicleta;
+
 /**
  * Hello world!
  *
@@ -11,7 +14,20 @@ public class App
     public static void main( String[] args )
     {
         Scanner = Scanner = new Scanner(System.in);
+        Bicicleta bicicleta = new Bicicleta();
+        int option;
     
+    do {
         System.out.println( "Escolha a opção desejada:\n 1 - Acelerar | 2 - Frear | 3 - Exibir status atual | 4 - Sair");
+        option = scanner.nextInt();
+        scanner.nextLine(); //buff clean
+
+        switch(option){
+            case 1:
+            System.out.println(bicicleta.acelerar());
+            break;
+        }
+        
+    }
     }
 }
